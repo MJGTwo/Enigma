05b7100c634ea0a5e27b85a77491e9ac6d87aa19
gardnm4 <gardnm4@rpi.edu>
2015-03-24 20:04:38 -0600

    Begin making of Rotors
    The Rotor will consist of three classes; a single letter, a row of
    letters, and then the rotter which contains two rows.

---
+++ Rotor.h
@@ -0,0 +1,40 @@
+#ifndef __ROTOR_H__
+#define __ROTOR_H__
+
+
+class Node{
+
+public:
+	char let;
+	Node* prev;
+	Node* next;
+	Node* crypt;
+
+
+
+};
+
+
+class Row{
+	Row();
+
+
+};
+
+
+
+class Rotor{
+
+public:
+
+	Rotor(int n);
+
+private:
+
+	
+
+};
+
+
+
+#endif
